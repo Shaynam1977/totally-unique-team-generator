f7e36e55fd08ab19652fbb1512f542329d372df2
Shayna Murphy <110637697+Shaynam1977@users.noreply.github.com>
2022-12-05 09:45:57 -0800

    added bootstrap

--- Source/createHTML.js
+++ Source/createHTML.js
@@ -4,11 +4,16 @@ function createHtml(employeeArray){
 
 function createManager(employee){
     return `
-    <h3>${employee.getrole()}</h3>
-    <h3>${employee.getofficeNumber()}</h3>
-    <h3>${employee.getname()}</h3>
-    <h3>${employee.getid()}</h3>
-    <h3>${employee.getemail()}</h3>
+    <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${employee.getrole()}</h5>
+    <p class="card-text">Employee Name: ${employee.getname()}</p>
+    <p class="card-text">Employee Office Number: ${employee.getofficeNumber()}</p>
+    <p class="card-text">Employee ID: ${employee.getid()}</p>
+    <a href="#" class="btn btn-primary">${employee.getemail()}</a>
+  </div>
+</div>
+   
     `
 }
 function createEngineer(employee){
@@ -38,6 +43,13 @@ employeeArray.forEach(teamMember => {
         cardArray.push(createManager(teamMember))
     }
     
+    else if(teamMember.getrole()==="Engineer"){
+        cardArray.push(createEngineer(teamMember))
+    }
+
+    else if(teamMember.getrole()==="Intern"){
+        cardArray.push(createIntern(teamMember))
+    }
     //create else/if conditions for if get role is a engineer or intern 
     //push result from function into card array
 });
@@ -56,6 +68,7 @@ module.exports=employeeArray=>{
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Team factory</title>
     <link rel="stylesheet" href="style.css" />
   </head>
